170cf2af4f464d8155fcf91bc7dff07f4e7e1def
Viktor Sec <hey@viktorsec.com>
2018-06-21 16:50:32 +0200

    Add react-router

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const Home = () => <h1>Home</h1>;
+const OtherPage = () => <h1>Other Page</h1>;
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/other-page" component={OtherPage} />
+        </div>
+      </Router>
+    </header>
+    <p className="App-intro">
+      Hey there. Go to the <a href="other-page">other page</a>. Pleb.
+    </p>
+  </div>
+);
 
 export default App;
