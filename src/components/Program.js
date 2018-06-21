7104701a896dde979efa3a93124f45a49a567e83
Viktor Sec <hey@viktorsec.com>
2018-06-22 01:31:30 +0200

    Fix issues raised in code review

--- src/components/Program.js
+++ src/components/Program.js
@@ -3,17 +3,18 @@ import { List, ListItem } from 'react-onsenui';
 import { schedule } from './consts/Schedule'
 
 const Program = () => (
-    <div>
-      <h3 className="page__title">Program</h3>
-      <List>
-        { schedule.map((item, i) => (
-            <ListItem key={i}>
-                <div className="list-item__label">{ item.time }</div>
-                { item.description }
-            </ListItem>
-        )) }
-      </List>
-    </div>
+  <div>
+    <h3 className="page__title">Program</h3>
+      <List
+        dataSource={schedule}
+        renderRow={(item, i) => (
+          <ListItem key={i}>
+            <div className="list-item__label">{ item.time }</div>
+              { item.description }
+          </ListItem>
+        )}
+      />
+  </div>
 );
 
 export default Program;
