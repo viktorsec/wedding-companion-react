4f2cb3dd77e9ee40302f2e6433d9a1c19e047cf4
Viktor Seč <hey@viktorsec.com>
2018-06-22 01:33:52 +0200

    Add program data (#11)
    * Add program data
    
    * Fix issues raised in code review

--- src/components/Program.js
+++ src/components/Program.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { List, ListItem } from 'react-onsenui';
+import { schedule } from './consts/Schedule'
 
 const Program = () => (
-    <div>
-      <h3 className="page__title">Program</h3>
-      <List>
-        <ListItem>
-          <div className="list-item__label">13:00</div>
-          Welcome drink
-        </ListItem>
-        <ListItem>
-          <div className="list-item__label">14:00</div>
-          Obrad
-        </ListItem>
-      </List>
-    </div>
+  <div>
+    <h3 className="page__title">Program</h3>
+      <List
+        dataSource={schedule}
+        renderRow={(item, i) => (
+          <ListItem key={i}>
+            <div className="list-item__label">{ item.time }</div>
+              { item.description }
+          </ListItem>
+        )}
+      />
+  </div>
 );
 
 export default Program;
---
+++ src/components/consts/Schedule.js
@@ -0,0 +1,54 @@
+export const schedule = [
+	{
+		"time": "13:00",
+		"description": "Príchod, welcome drink"
+	},
+	{
+		"time": "14:00",
+		"description": "Svadobný obrad"
+	},
+	{
+		"time": "15:00",
+		"description": "Obed",
+	},
+	{
+		"time": "16:00",
+		"description": "Krájanie torty"
+	},
+	{
+		"time": "16:30",
+		"description": "Prehliadka vinárstva"
+	},
+	{
+		"time": "18:00",
+		"description": "Hudobný hosť"
+	},
+	{
+		"time": "19:00",
+		"description": "Topánková hra"
+	},
+	{
+		"time": "19:30",
+		"description": "Prvý tanec mladomanželov a zahájenie diskotéky"
+	},
+	{
+		"time": "20:30",
+		"description": "Spoločné fotenie pri západe slnka"
+	},
+	{
+		"time": "21:00",
+		"description": "Otvorenie večerného bufetu"
+	},
+	{
+		"time": "21:15",
+		"description": "Kvíz"
+	},
+	{
+		"time": "22:45",
+		"description": "Svetielka prianí"
+	},
+	{
+		"time": "23:00",
+		"description": "Kapustnica a voľná zábava"
+	}
+]
