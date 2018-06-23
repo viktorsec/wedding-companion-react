35c9db2fc77e76b832d9c6ba1dd24e014b6110e7
Viktor Sec <hey@viktorsec.com>
2018-06-23 07:54:37 +0200

    Add tutorial for Android

--- src/components/Help.js
+++ src/components/Help.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const Help = () => (
-    <div>
+    <div style={{height: '1686px'}} className="container__center">
+        <h1>iOS</h1>
         <img className="img-fluid" src="tutorial.png" alt="tutorial" />
+        <h1>Android</h1>
+        <img className="img-fluid" src="tutorial-android.png" alt="tutorial" />
     </div>
 );
 
